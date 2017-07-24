8953ae1be63ee2bb271e24f23656cbb3c1fe1708
DongYuanLv <dongyuanlv@126.com>
2017-07-24 19:30:35 +0800

    guanquan

--- camera/camera/camera.cpp
+++ camera/camera/camera.cpp
@@ -2,13 +2,13 @@
 //
 
 #include "stdafx.h"
-void addpictuer(Mat & src1, double alpha, Mat & src2, double beta, Mat &dst);
-void bold(const Mat & src1, Mat & src2);
+void bold(const Mat &src, Mat & dst);
+void DrawCircle(const Mat src, Mat dst, Point center, int radius, int thickness);
 
 int main()
 {
-	VideoCapture capture(0);
-
+	VideoCapture capture(1);
+	int frameRad = 12;
 	//capture.set(CAP_PROP_FRAME_WIDTH, 1280);
 	//capture.set(CAP_PROP_FRAME_HEIGHT, 720);
 
@@ -24,37 +24,41 @@ int main()
 	{   
 		double dProcessTime = static_cast<double>(getTickCount());
 	
-		Mat frame,framegray,framemake, framemakewide,framenew;
+		Mat frame,framegray,framemake,framenew, framemakewide;
+		Mat kernel = (Mat_<float>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+		//int min = 100;
+		//int max = 500;
+
+		vector<vector<Point>> frameContours;
+		vector<Vec4i> frameHierarchy;
 
         capture >> frame;
 		
 		cvtColor(frame, framegray, COLOR_BGR2GRAY);
-
-		//blur(framegray, framegray, Size(3, 3));
 		GaussianBlur(framegray, framegray, Size(7, 7), 1.3, 1.3);
 
 		Canny(framegray, framemake, 30,90, 3);
 
-		Mat element = getStructuringElement(MORPH_RECT, Size(2, 2));
+//		bold(framemake, framemakewide);
 
-		morphologyEx(framemake, framemakewide, MORPH_DILATE, element);
+		cvtColor(framemake, framemakewide, CV_GRAY2BGR);
 
-		//bold(framemake, framemakewide);
-
-		cvtColor(framemakewide, framemakewide, CV_GRAY2BGR);
 		addWeighted(frame, 1.0, framemakewide, 1.0, 0.0, framenew);
-		//addpictuer(frame, 1, framemakewide, 1.0, framenew);
 
+	    filter2D(framenew, framenew, framenew.depth(), kernel);  //锐化处理
 
+		if (frameRad > sqrt(framenew.cols*framenew.cols+ framenew.rows*framenew.rows)/2+4)
+			frameRad = 12;
+		DrawCircle(framemakewide, framenew, Point(framenew.cols / 2, framenew.rows / 2), frameRad, -1);
+		frameRad = frameRad + 12;
 		dProcessTime = (static_cast<double>(getTickCount()) - dProcessTime) / getTickFrequency();
-		double fps = 1.0 / dProcessTime;
 
 		imshow("photo", frame);
 		imshow("photonew", framenew);
 
 		nKey = waitKey(1);
 		cout << dProcessTime << endl;
-		//cout << "Frame Width:" << frame.cols << "\tFrame Height:" << frame.rows << endl;
+		cout << "Frame Width:" << frame.cols << "\tFrame Height:" << frame.rows << endl;
 
 	}
 
@@ -63,44 +67,19 @@ int main()
 
 //--------------------------------融合函数----------------------------------
 
-void addpictuer(Mat & src1, double alpha, Mat & src2, double beta, Mat &dst)
+void bold(const Mat &src, Mat & dst)
 {
-	if ((src1.empty()) && (src2.empty()))
-	{
-		exit(-1);
-	}
+	 dst.create(src.size(), src.type());
 
-	dst.create(src1.size(), src1.type());
-
-	for (int j = 0; j < src1.rows; j++) {
-		Vec3b* data1 = src1.ptr<Vec3b>(j);
-		uchar* data2 = src2.ptr<uchar>(j);
-		Vec3b* data3 = dst.ptr<Vec3b>(j);
-		for (int i = 0; i < src1.cols; i++) {
-
-			data3[i][0] = saturate_cast<uchar>(alpha*data1[i][0] + beta*data2[i]);
-			data3[i][1] = saturate_cast<uchar>(alpha*data1[i][1] + beta*data2[i]);
-			data3[i][2] = saturate_cast<uchar>(alpha*data1[i][2] + beta*data2[i]);
-
-		}
-	}
-
-}
-void bold(const Mat & src1, Mat & src2)
-{
-	src2.create(src1.size(), src1.type());
-
-	for (int j = 0; j < src1.rows; j++) {
-		const uchar* data1 = src1.ptr<uchar>(j);
-		uchar* data2 = src2.ptr<uchar>(j);
-		for (int i = 0; i < src1.cols; i++) {
-			if (data1[i] == 255)
+	for (int j = 0; j < src.rows; j++) {
+		const uchar* data1 = src.ptr<uchar>(j);
+	    uchar* data2 = dst.ptr<uchar>(j);
+		for (int i = 0; i < src.cols; i++) {
+			if (255 ==data1[i] )
 			{
-				data2[i - 2] = 255;
 				data2[i - 1] = 255;
 				data2[i] = 255;
 				data2[i + 1] = 255;
-				data2[i + 2] = 255;
 			}
 			else
 			{
@@ -109,6 +88,13 @@ void bold(const Mat & src1, Mat & src2)
 
 		}
 	}
-
-
 }
+void DrawCircle(const Mat src, Mat dst, Point center,int radius,int thickness)
+{   
+	Mat backgound;
+	backgound.create(dst.rows, dst.cols, dst.type());
+	circle(backgound, center, radius+100, Scalar(255,0,0), thickness,8);
+	circle(backgound, center, radius+50, Scalar(0, 255, 0), thickness, 8);
+	circle(backgound, center, radius, Scalar(0, 0, 255), thickness, 8);
+	backgound.copyTo(dst,src);
+}  
